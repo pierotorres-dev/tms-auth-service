76d86e5dabb0d50d252d1e065ce1aa1f5df1b3b7
Piero Torres <torresartica1701@gmail.com>
2025-04-30 12:48:50 -0500

    feat: add jwt provider

---
+++ src/main/java/com/dliriotech/tms/authservice/security/jwt/JwtProvider.java
@@ -0,0 +1,63 @@
+package com.dliriotech.tms.authservice.security.jwt;
+
+import com.dliriotech.tms.authservice.entity.AuthUser;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtProvider {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private long expirationMs;
+
+    private Key getSigningKey() {
+        return Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
+    public String createToken(AuthUser user) {
+        var now = new Date();
+        var expiryDate = new Date(now.getTime() + expirationMs);
+
+        return Jwts.builder()
+                .setSubject(user.getUserName())
+                .claim("id", user.getId())
+                .claim("role", user.getRole())
+                .claim("id_empresa", user.getIdEmpresa())
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public boolean validate(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(getSigningKey())
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public String getUserNameFromToken(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        return claims.getSubject();
+    }
+
+}
